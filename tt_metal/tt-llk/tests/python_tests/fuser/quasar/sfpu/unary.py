bc22f7b7f98343c6fd66b34e9495304f804fef43
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2026-07-27 21:48:35 +0000

    quasar fuser with semaphores

--- tt_metal/tt-llk/tests/python_tests/fuser/quasar/sfpu/unary.py
+++ tt_metal/tt-llk/tests/python_tests/fuser/quasar/sfpu/unary.py
@@ -85,9 +85,9 @@ class UnarySfpu(Sfpu):
         op = f"SfpuType::{self.operation.cpp_enum_value}"
 
         return (
-            f"    // Operation {stage}: Unary {self.operation.cpp_enum_value} SFPU\n"
-            f"    _llk_math_eltwise_sfpu_init_();\n"
-            f"    test_utils::init_unary_sfpu_operation_quasar<{op}>();\n"
+            f"// Operation {stage}: Unary {self.operation.cpp_enum_value} SFPU\n"
+            f"_llk_math_eltwise_sfpu_init_();\n"
+            f"test_utils::init_unary_sfpu_operation_quasar<{op}>();\n"
         )
 
     def calculate(
@@ -101,9 +101,10 @@ class UnarySfpu(Sfpu):
         dest_sync = operation.dest_sync.cpp_enum_value
         en_32bit_dest = config.dest_acc.cpp_enum_value
         sfpu_format = config.sentinel._math_format.cpp_enum_value
+        quasar_iterations = self.iterations // 4
         return (
-            f"    test_utils::call_unary_sfpu_operation_quasar<"
-            f"{op}, {dest_sync}, {en_32bit_dest}, false, {self.iterations}"
+            f"test_utils::call_unary_sfpu_operation_quasar<"
+            f"{op}, {dest_sync}, {en_32bit_dest}, false, {quasar_iterations}"
             f">({self.dest_idx}, {sfpu_format});\n"
         )
 
