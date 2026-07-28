b220a319579f532ba0f6a9ce4af7a1bc92d53c9c
KalaivaniMCW <kbaskar@tenstorrent.com>
2026-07-28 13:14:45 +0000

    Update Quasar llk with ROUND_NEAREST flag

--- tt_metal/tt-llk/tests/helpers/include/sfpu_operations_quasar.h
+++ tt_metal/tt-llk/tests/helpers/include/sfpu_operations_quasar.h
@@ -320,7 +320,7 @@ void init_binary_sfpu_operation_quasar()
  * @param math_format Dest math format (Int32 vs float path for MUL and max/min).
  * @note Must be preceded by @ref init_binary_sfpu_operation_quasar for the same op.
  */
-template <ckernel::BinaryOp OP, DstSync DST_SYNC, bool is_fp32_dest_acc_en, int ITERATIONS = SFPU_ITERATIONS>
+template <ckernel::BinaryOp OP, DstSync DST_SYNC, bool is_fp32_dest_acc_en, bool ROUND_NEAREST = false, int ITERATIONS = SFPU_ITERATIONS>
 void call_binary_sfpu_operation_quasar(std::uint32_t src0_tile, std::uint32_t src1_tile, std::uint32_t dst_tile, [[maybe_unused]] DataFormat math_format)
 {
     if constexpr (OP == BinaryOp::ADD)
@@ -336,7 +336,7 @@ void call_binary_sfpu_operation_quasar(std::uint32_t src0_tile, std::uint32_t sr
                 DST_SYNC,
                 is_fp32_dest_acc_en,
                 calculate_sfpu_binary,
-                (false /*APPROX*/, BinaryOp::ADD, is_fp32_dest_acc_en, ITERATIONS),
+                (false /*APPROX*/, BinaryOp::ADD, is_fp32_dest_acc_en, ROUND_NEAREST, ITERATIONS),
                 src0_tile,
                 src1_tile,
                 dst_tile,
@@ -350,7 +350,7 @@ void call_binary_sfpu_operation_quasar(std::uint32_t src0_tile, std::uint32_t sr
             DST_SYNC,
             is_fp32_dest_acc_en,
             calculate_sfpu_binary,
-            (false /*APPROX*/, BinaryOp::SUB, is_fp32_dest_acc_en, ITERATIONS),
+            (false /*APPROX*/, BinaryOp::SUB, is_fp32_dest_acc_en, ROUND_NEAREST, ITERATIONS),
             src0_tile,
             src1_tile,
             dst_tile,
--- tt_metal/tt-llk/tests/python_tests/fuser/quasar/sfpu/binary.py
+++ tt_metal/tt-llk/tests/python_tests/fuser/quasar/sfpu/binary.py
@@ -25,6 +25,7 @@ class BinarySfpu(Sfpu):
         self,
         operation: MathOperation,
         approx_mode: ApproximationMode = ApproximationMode.No,
+        round_nearest: bool = False,
         iterations: int = 8,
         dst_index_in0: int = 0,
         dst_index_in1: int = 1,
@@ -36,6 +37,7 @@ class BinarySfpu(Sfpu):
             )
         self.operation = operation
         self.approx_mode = approx_mode
+        self.round_nearest = round_nearest
         self.iterations = iterations
         self.dst_index_in0 = dst_index_in0
         self.dst_index_in1 = dst_index_in1
@@ -103,10 +105,11 @@ class BinarySfpu(Sfpu):
         src1 = self.dst_index_in0
         src2 = self.dst_index_in1
         dst = self.dst_index_out
+        round_nearest = "true" if self.round_nearest else "false"
 
         return (
             f"    test_utils::call_binary_sfpu_operation_quasar<"
-            f"{op}, {dest_sync}, {en_32bit_dest}, {self.iterations}"
+            f"{op}, {dest_sync}, {en_32bit_dest}, {round_nearest}, {self.iterations}"
             f">({src1} /* src0_tile */, {src2} /* src1_tile */, {dst} /* dst_tile */, "
             f"{config.sentinel.math_format});\n"
         )
